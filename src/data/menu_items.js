98283f27aef4766fca702d6854921835dee2f589
ddoris2106 <daundre.doris16@myhunter.cuny.edu>
2023-06-21 21:02:44 -0400

    Added menu items in sections, beverages, and add on ingredients

---
+++ src/data/menu_items.js
@@ -0,0 +1,255 @@
+const menuItems = {
+	"Traditional Pizzas": [
+		{
+			name: "Cheese Slice",
+			price: 2.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Sicilian Slice",
+			price: 2.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Small Pizza",
+			price: 13.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Medium Pizza",
+			price: 15.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Large Pizza",
+			price: 19.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Chicago Deep Dish Pizza",
+			price: 19.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Grandma Thin Crust Sicilian Pizza",
+			price: 19.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Baby Pizza",
+			price: 6.99,
+			description: "",
+			image: "",
+		},
+	],
+	"Specialty Pizzas": [
+		{
+			name: "Barbeque Chicken Slice",
+			price: 4.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Veggie Slice",
+			price: 4.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Buffalo Chicken Slice",
+			price: 4.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Chicken Feast",
+			price: 25.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Veggie Feast",
+			price: 27.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Meat Lovers Feast",
+			price: 27.99,
+			description: "",
+			image: "",
+		},
+	],
+	Combos: [
+		{
+			name: "Gyro Combo",
+			price: 10.99,
+			description:
+				"Juicy Beef or Chicken Gyro with French Fries and a Soda",
+			image: "",
+		},
+		{
+			name: "Mozzarella Sticks Combo",
+			price: 9.99,
+			description:
+				"6 Crispy and cheesy Mozzarella Sticks with French Fries and a Soda",
+			image: "",
+		},
+		{
+			name: "Buffalo Wings Combo",
+			price: 13.99,
+			description: "10 Buffalo Wings with French Fries and a Soda",
+			image: "",
+		},
+		{
+			name: "Buffalo Wings, Mozzarella Sticks Combo",
+			price: 13.99,
+			description: "8 Buffalo Wings with 4 Mozzarella Sticks and a Soda",
+			image: "",
+		},
+		{
+			name: "Philly Cheasesteak Combo",
+			price: 10.99,
+			description:
+				"Philly Cheesesteak with Peppers and Onions served with French Fries and a Soda",
+			image: "",
+		},
+		{
+			name: "Cheeseburger Combo",
+			price: 9.99,
+			description: "Cheeseburger with French Fries and Soda",
+			image: "",
+		},
+		{
+			name: "Spicy Chicken Sandwich Combo",
+			price: 9.99,
+			description: "Spicy Chicken Sandwich with French Fries and Soda",
+			image: "",
+		},
+		{
+			name: "Chicken Tenders Combo",
+			price: 9.99,
+			description: "8 Chicken Tenders with French Fries and Soda",
+			image: "",
+		},
+		{
+			name: "Chicken Tenders, Mozzarella Sticks Combo",
+			price: 9.99,
+			description: "6 Chicken Tenders and 4 Mozzarella Sticks with Soda",
+			image: "",
+		},
+	],
+	Pasta: [
+		{
+			name: "Penne Vodka",
+			price: 9.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Fettucini Alfredo",
+			price: 9.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Lasagna",
+			price: 13.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Spaghetti and Meatballs",
+			price: 12.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Stuffed Shells",
+			price: 9.99,
+			description:
+				"Shell Pasta stuffed with Ricotta and Mozzarella Cheese, Includes Fresh Italian Bread",
+			image: "",
+		},
+	],
+	Specials: [
+		{
+			name: "Potato Knish",
+			price: 2.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Garlic Knots",
+			price: 4.99,
+			description: "6 fresh garlic knots with cheese",
+			image: "",
+		},
+		{
+			name: "Calzone",
+			price: 6.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Chicken Roll",
+			price: 5.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Sausage Roll",
+			price: 5.99,
+			description: "",
+			image: "",
+		},
+		{
+			name: "Beef Patty",
+			price: 2.99,
+			description: "",
+			image: "",
+		},
+	],
+};
+
+const addOns = {
+	addOnPrice: 2.99,
+	meat: ["Chicken", "Turkey", "Ground Beef", "Beef Sausage"],
+	vegetables: [
+		"Peppers and Onions",
+		"Pineapples",
+		"Olives",
+		"Mushrooms",
+		"Jalepenos",
+	],
+	other: ["cheese"],
+};
+
+const beverages = {
+	soda: ["Sprite", "Fanta", "Pepsi", "Coca Cola", "Root Beer"],
+	sodaPrice: {
+		small: 1.99,
+		large: 3.99,
+	},
+	drinks: [
+		"Cherry Slushy",
+		"Strawberry Slushy",
+		"Lemon Lime Slushy",
+		"Grape Slushy",
+		"Kiwi Strawberry Slushy",
+		"Lemonade",
+		"Pink Lemonade",
+		"Fruit Punch",
+	],
+	drinkPrice: {
+		small: 2.99,
+		large: 4.99,
+	},
+};
